b7c50f599a90adb8dbf73f0ccc781f81296aed2d
marlisajp <marlisajp@gmail.com>
2024-03-12 23:47:33 -0400

    added game card skeleton

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -24,22 +24,27 @@ interface FetchGamesResponse {
 const useGames = () => {
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const controller = new AbortController();
-
+    setIsLoading(true);
     apiClient
       .get<FetchGamesResponse>('/games', { signal: controller.signal })
-      .then((res) => setGames(res.data.results))
+      .then((res) => {
+        setGames(res.data.results);
+        setIsLoading(false);
+      })
       .catch((e) => {
         if (e instanceof CanceledError) return;
         setError(e.message);
+        setIsLoading(false);
       });
 
     return () => controller.abort();
   }, []);
 
-  return { games, setGames, error, setError };
+  return { games, setGames, error, setError, isLoading };
 };
 
 export default useGames;
