242abae549222a6f09533aeb392bdbf1878e8e07
marlisajp <marlisajp@gmail.com>
2024-03-13 19:03:36 -0400

    added env variable for apikey

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
+const apiKey = import.meta.env.VITE_RAWG_API_KEY;
+
 export default axios.create({
   baseURL: 'https://api.rawg.io/api',
   params: {
-    key: 'fd156d98b5d447c8982a843da525fe22',
+    key: apiKey,
   },
 });
